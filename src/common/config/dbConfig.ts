ec9e65eba6920070a5496f0f51cf79cf5b669fad
briankopp <bmkopp10@gmail.com>
2024-08-29 00:17:15 -0500

    connect to live db

--- src/common/config/dbConfig.ts
+++ src/common/config/dbConfig.ts
@@ -1,43 +1,15 @@
 import mongoose from 'mongoose';
-import { MongoMemoryServer } from 'mongodb-memory-server';
 
-// Initialize MongoMemoryServer
-const mongoServer = new MongoMemoryServer();
+const mongoURI = 'mongodb+srv://doadmin:j964W0xv831C2qdo@db-mongodb-nyc3-10354-1736305b.mongo.ondigitalocean.com/admin?retryWrites=true&w=majority'
 
 export const connect = async () => {
-  // Ensure the server is started
-  await mongoServer.start();
 
-  // Get the URI for connecting to MongoDB
-  const uri = mongoServer.getUri();
-  
-  // Connect to MongoDB using Mongoose
-  await mongoose.connect(uri, {
-    // Use default options for the latest Mongoose versions
-  });
+  await mongoose.connect(mongoURI, {});
 
-  console.log('In-memory MongoDB connected');
+  console.log('MongoDB connected');
 };
 
 export const close = async () => {
   await mongoose.disconnect();
-  await mongoServer.stop();
-  console.log('In-memory MongoDB connection closed');
+  console.log('MongoDB connection closed');
 };
-
-
-// import mongoose from 'mongoose';
-// import { MongoMemoryServer } from 'mongodb-memory-server';
-
-// const mongoServer = new MongoMemoryServer();
-
-// export const connect = async () => {
-//   const uri = await mongoServer.getUri();
-//   await mongoose.connect(uri, {});
-//   console.log('In-memory MongoDB connected');
-// };
-
-// export const close = async () => {
-//   await mongoose.disconnect();
-//   await mongoServer.stop();
-// };
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,30 @@
 import app from './app';
+import os from 'os';
 
 const port = process.env.PORT || 3000;
 
+// Function to get local IP address
+function getLocalIP() {
+  const interfaces = os.networkInterfaces();
+  let ipAddress = '';
+
+  for (const name of Object.keys(interfaces)) {
+    // @ts-ignore
+    for (const net of interfaces[name]) {
+      if (net.family === 'IPv4' && !net.internal) {
+        ipAddress = net.address;
+        break;
+      }
+    }
+    if (ipAddress) break;
+  }
+
+  return ipAddress;
+}
+
+// Get the local IP address
+const ip = getLocalIP();
+
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server is running at http://${ip}:${port}`);
 });
